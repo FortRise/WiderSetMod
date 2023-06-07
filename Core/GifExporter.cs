ca72d56db04097fadd63435d7df7495bf169bf66
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-06-07 22:08:51 +0800

    GifExporter done

--- Core/GifExporter.cs
+++ Core/GifExporter.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Reflection;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
@@ -18,12 +17,66 @@ namespace EightPlayerMod
                 GifExporterExportGif_patch
             );
             IL.TowerFall.GifExporter.Render += GifExporterRender_patch;
+            IL.Moments.Encoder.GifEncoder.GetImagePixels += GifEncoderGetImagePixels_patch;
+            IL.Moments.Encoder.GifEncoder.AddFrame += GifEncoderAddFrame_patch;
         }
 
         public static void Unload() 
         {
             hook_ExportGif.Dispose();
             IL.TowerFall.GifExporter.Render -= GifExporterRender_patch;
+            IL.Moments.Encoder.GifEncoder.GetImagePixels -= GifEncoderGetImagePixels_patch;
+            IL.Moments.Encoder.GifEncoder.AddFrame -= GifEncoderAddFrame_patch;
+        }
+
+        private static void GifEncoderAddFrame_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcI4(320))) 
+            {
+                cursor.EmitDelegate<Func<int, int>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 420;
+                    return size;
+                });
+            }
+        }
+
+        private static void GifEncoderGetImagePixels_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcI4(230400))) 
+            {
+                cursor.EmitDelegate<Func<int, int>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 302400;
+                    return size;
+                });
+            }
+
+            cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcI4(76800))) 
+            {
+                cursor.EmitDelegate<Func<int, int>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 100800;
+                    return size;
+                });
+            }
+
+            cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(320))) 
+            {
+                cursor.EmitDelegate<Func<int, int>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 420;
+                    return size;
+                });
+            }
         }
 
         private static void GifExporterRender_patch(ILContext ctx)
