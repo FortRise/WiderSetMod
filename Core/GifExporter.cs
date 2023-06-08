f3e91c71a2e32417a16f3a2d0388bfd59269d38c
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-06-08 18:58:40 +0800

    GifExporter

--- Core/GifExporter.cs
+++ Core/GifExporter.cs
@@ -7,6 +7,87 @@ using TowerFall;
 
 namespace EightPlayerMod 
 {
+    public static class ReplayViewerPatch 
+    {
+        public static void Load() 
+        {
+            IL.TowerFall.ReplayViewer.ctor += ctor_patch;
+            IL.TowerFall.ReplayViewer.Render += Render_patch;
+            IL.TowerFall.ReplayViewer.PostScreenRender += PostScreenRender_patch;
+        }
+
+        public static void Unload() 
+        {
+            IL.TowerFall.ReplayViewer.ctor -= ctor_patch;
+            IL.TowerFall.ReplayViewer.Render -= Render_patch;
+            IL.TowerFall.ReplayViewer.PostScreenRender -= PostScreenRender_patch;
+        }
+
+        private static void Render_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(320))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 420;
+                    return size;
+                });
+            }
+
+            cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(180))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 230;
+                    return size;
+                });
+            }
+        }
+
+        private static void PostScreenRender_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(160))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 210;
+                    return size;
+                });
+            }
+
+            cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(310))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 410;
+                    return size;
+                });
+            }
+        }
+
+        private static void ctor_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcI4(320))) 
+            {
+                cursor.EmitDelegate<Func<int, int>>(size => {
+                    if (EightPlayerModule.IsEightPlayer)
+                        return 420;
+                    return size;
+                });
+            }
+        }
+    }
+
     public static class GifExporterPatch 
     {
         private static IDetour hook_ExportGif;
