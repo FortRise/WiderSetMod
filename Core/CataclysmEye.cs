8c64676eb9e9f8b6d85a466a4e4917a52377f79f
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-06-24 23:17:51 +0800

    Cataclysm boss

---
+++ Core/CataclysmEye.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Reflection;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using TowerFall;
+
+namespace EightPlayerMod 
+{
+    public static class CataclysmEyePatch 
+    {
+        private static IDetour hook_IdleSafeCoroutine;
+        private static IDetour hook_DeadCoroutine;
+        private static IDetour hook_DeadCoroutine91_0b__1;
+        public static void Load() 
+        {
+            IL.TowerFall.CataclysmEye.ctor += MiddlePatch;
+            IL.TowerFall.CataclysmEye.IdleFreakoutEnter += IdleFreakoutEnter_patch;
+            IL.TowerFall.CataclysmEye.DoSlam += MiddlePatch;
+            IL.TowerFall.CataclysmBlade.Update += CataclysmBladeUpdate_patch;
+            IL.TowerFall.CataclysmBullet.Update += CataclysmBulletUpdate_patch;
+            hook_IdleSafeCoroutine = new ILHook(
+                typeof(CataclysmEye).GetMethod("IdleSafeCoroutine", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetStateMachineTarget(),
+                MiddlePatchSafe
+            );
+            hook_DeadCoroutine = new ILHook(
+                typeof(CataclysmEye).GetMethod("DeadCoroutine", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetStateMachineTarget(),
+                MiddlePatchSafe
+            );
+            hook_DeadCoroutine91_0b__1 = new ILHook(
+                typeof(CataclysmEye).GetNestedType("<>c__DisplayClass91_0", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetMethod("<DeadCoroutine>b__1", BindingFlags.Instance | BindingFlags.NonPublic),
+                MiddlePatch
+            );
+        }
+
+        public static void Unload() 
+        {
+            IL.TowerFall.CataclysmEye.ctor -= MiddlePatch;
+            IL.TowerFall.CataclysmEye.IdleFreakoutEnter -= IdleFreakoutEnter_patch;
+            IL.TowerFall.CataclysmEye.DoSlam -= MiddlePatch;
+            IL.TowerFall.CataclysmBlade.Update -= CataclysmBladeUpdate_patch;
+            IL.TowerFall.CataclysmBullet.Update -= CataclysmBulletUpdate_patch;
+            hook_IdleSafeCoroutine.Dispose();
+            hook_DeadCoroutine.Dispose();
+            hook_DeadCoroutine91_0b__1.Dispose();
+        }
+
+        private static void CataclysmBulletUpdate_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(330f))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(x => {
+                    if (EightPlayerModule.IsEightPlayer) 
+                    {
+                        return 420 + 10;
+                    }
+                    return x;
+                });
+            }
+        }
+
+        private static void CataclysmBladeUpdate_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(295f))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(x => {
+                    if (EightPlayerModule.IsEightPlayer) 
+                    {
+                        return 420 - 25;
+                    }
+                    return x;
+                });
+            }
+        }
+
+        private static void IdleFreakoutEnter_patch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(160f))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(x => {
+                    if (EightPlayerModule.IsEightPlayer) 
+                    {
+                        return 420 / 2;
+                    }
+                    return x;
+                });
+            }
+
+            cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(190f))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(x => {
+                    if (EightPlayerModule.IsEightPlayer) 
+                    {
+                        return 240;
+                    }
+                    return x;
+                });
+            }
+        }
+
+        private static void MiddlePatchSafe(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(160f))) 
+            {
+                if (cursor.Next.MatchLdcR4(1))  
+                    continue;
+                cursor.EmitDelegate<Func<float, float>>(x => {
+                    if (EightPlayerModule.IsEightPlayer) 
+                    {
+                        return 420 / 2;
+                    }
+                    return x;
+                });
+            }
+        }
+
+        private static void MiddlePatch(ILContext ctx)
+        {
+            var cursor = new ILCursor(ctx);
+
+            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdcR4(160f))) 
+            {
+                cursor.EmitDelegate<Func<float, float>>(x => {
+                    if (EightPlayerModule.IsEightPlayer) 
+                    {
+                        return 420 / 2;
+                    }
+                    return x;
+                });
+            }
+        }
+    }
+}
